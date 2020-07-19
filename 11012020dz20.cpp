b3194b9acbe10ea79ca9816e3319c2fb91fff3ca
blazhkevych <65856963+blazhkevych@users.noreply.github.com>
2020-07-19 18:31:31 +0300

    Добавьте файлы проекта.

---
+++ 11012020dz20.cpp
@@ -0,0 +1,15 @@
+﻿/*
+* Пользователь 	вводит с клавиатуры число.Вывести на 	экран символ с соответствующим кодом.
+*/
+#include <iostream>
+#include <Windows.h>;
+using namespace std;
+int main()
+{
+	SetConsoleOutputCP(1251);
+	cout << "Введите число с клавиатуры: ";
+	int a;
+	cin >> a;
+	cout << char(a) << " имеет ASCII код " << static_cast<int>(a) << endl;
+	return 0;
+}
